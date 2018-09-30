b126426c1e2f2399b1704bffc712f768d3c8e777
Xiaowei <zhan1027@umn.edu>
2018-09-30 13:56:23 -0500

    Leapyear lab is done

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,13 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Xiaowei
  */
 public class LeapYear {
+    //this is to define the leapyear method
+    public static boolean isLeapYear(int year){
+        if (year % 400 == 0) {return true;}
+        else if (year % 4 == 0 & year % 100 != 0) {return true;}
+        else {return false;}
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
@@ -30,4 +36,3 @@ public class LeapYear {
         }
     }
 }
-
